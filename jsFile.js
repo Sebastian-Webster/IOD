eae12a1e960284b0445ab523f293242f6d35ab83
Sebastian Webster <coolman16738@gmail.com>
2022-07-05 17:06:51 +1200

    make some changes

--- jsFile.js
+++ jsFile.js
@@ -5,3 +5,9 @@ function sayText(text) {
 function changeTextOfButton() {
     document.getElementById('changeTextButton').innerHTML = document.getElementById('changeTextButton').innerHTML == 'Click me to change the text!' ? 'I am a cool button! Click me again!' : 'Click me to change the text!';
 }
+
+function doAddition() {
+    const number1 = prompt('Enter the first number to add.')
+    const number2 = prompt('Enter the second number to add.')
+    document.getElementById('additionResult').innerHTML = number1 + ' + ' + number2 + ' = ' + (parseInt(number1) + parseInt(number2));
+}
